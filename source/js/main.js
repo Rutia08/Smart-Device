715d80a74de0fd4bc989a01b4100f88b95efa9a0
Rutia08 <rutia08@gmail.com>
2022-10-23 23:07:00 +0300

    добюавялет маску поля ввода телефона

--- source/js/main.js
+++ source/js/main.js
@@ -5,6 +5,41 @@ import {initModals} from './modules/modals/init-modals';
 
 window.addEventListener('DOMContentLoaded', () => {
 
+  //маска для поля ввода телефона (https://web-revenue.ru/verstka/maska-vvoda-telefona-v-input-na-js)
+  [].forEach.call( document.querySelectorAll('[data-phone-input]'), function(input) {
+    var keyCode;
+    function mask(event) {
+        event.keyCode && (keyCode = event.keyCode);
+        var pos = this.selectionStart;
+        if (pos < 3) event.preventDefault();
+        var matrix = "+7 (___) ___ ____",
+            i = 0,
+            def = matrix.replace(/\D/g, ""),
+            val = this.value.replace(/\D/g, ""),
+            new_value = matrix.replace(/[_\d]/g, function(a) {
+                return i < val.length ? val.charAt(i++) || def.charAt(i) : a
+            });
+        i = new_value.indexOf("_");
+        if (i != -1) {
+            i < 5 && (i = 3);
+            new_value = new_value.slice(0, i)
+        }
+        var reg = matrix.substr(0, this.value.length).replace(/_+/g,
+            function(a) {
+                return "\\d{1," + a.length + "}"
+            }).replace(/[+()]/g, "\\$&");
+        reg = new RegExp("^" + reg + "$");
+        if (!reg.test(this.value) || this.value.length < 5 || keyCode > 47 && keyCode < 58) this.value = new_value;
+        if (event.type == "blur" && this.value.length < 5)  this.value = ""
+    }
+
+    input.addEventListener("input", mask, false);
+    input.addEventListener("focus", mask, false);
+    input.addEventListener("blur", mask, false);
+    input.addEventListener("keydown", mask, false)
+
+  });
+
   // Utils
   // ---------------------------------
 
@@ -60,3 +95,4 @@ aboutButton.addEventListener('click', function() {
   aboutButton.innerHTML === 'Подробнее' ? aboutButton.innerHTML = 'Скрыть': aboutButton.innerHTML = 'Подробнее'
   aboutAdditional.classList.toggle('is-hidden');
 })
+
